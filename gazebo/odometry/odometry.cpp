1d2872b56d899505a7f3b0426223dccde7e5976a
Wataru UEMURA <wataru@kdel.org>
2023-06-12 23:10:37 +0900

    publish the odometry via ROS.

---
+++ gazebo/odometry/odometry.cpp
@@ -0,0 +1,260 @@
+/***************************************************************************
+ *  odometry.cpp - provides odometry simulation of object model
+ *
+ *  Created: Fri Feb 20 18:05:07 2015
+ *  Copyright  2015 Stefan Profanter
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include "odometry.h"
+
+#include <utils/misc/gazebo_api_wrappers.h>
+
+#include <math.h>
+
+using namespace gazebo;
+
+// Register this plugin to make it available in the simulator
+GZ_REGISTER_MODEL_PLUGIN(Odometry)
+
+///Constructor
+Odometry::Odometry()
+{
+}
+///Destructor
+Odometry::~Odometry()
+{
+	printf("Destructing Odometry Plugin!\n");
+}
+
+/** on loading of the plugin
+ * @param _parent Parent Model
+ */
+void
+Odometry::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+{
+	// Store the pointer to the model
+	this->model_ = _parent;
+
+	estimate_x     = 0;
+	estimate_y     = 0;
+	estimate_omega = 0;
+
+	last_sent_time_ = model_->GetWorld()->GZWRAP_SIM_TIME().Double();
+
+	//get the model-name
+	this->name_ = model_->GetName();
+	printf("Loading Odometry Plugin of model %s\n", name_.c_str());
+
+	// Listen to the update event. This event is broadcast every
+	// simulation iteration.
+	this->update_connection_ =
+	  event::Events::ConnectWorldUpdateBegin(boost::bind(&Odometry::OnUpdate, this, _1));
+
+	//Create the communication Node for communication with fawkes
+	this->node_ = transport::NodePtr(new transport::Node());
+
+	//the namespace is set to the model name!
+	this->node_->Init(model_->GetWorld()->GZWRAP_NAME() + "/" + name_);
+
+	//init last sent time
+	last_sent_time_ = model_->GetWorld()->GZWRAP_SIM_TIME().Double();
+
+	//create publisher
+	this->odometry_pub_ = this->node_->Advertise<msgs::Vector3d>("~/RobotinoSim/Odometry/");
+
+	//create subscriber
+	this->set_odometry_sub_ = this->node_->Subscribe(std::string("~/RobotinoSim/SetOdometry/"),
+	                                                 &Odometry::on_set_odometry_msg,
+	                                                 this);
+
+		// subscriber for ROS
+	// Initialize ros, if it has not already bee initialized.
+	std::string clientName= "gazsim_odom" + this->model_->GetName();
+	if (!ros::isInitialized())
+	{
+	  int argc = 0;
+	  char **argv = NULL;
+	  ros::init(argc, argv, clientName.c_str(),
+	      ros::init_options::NoSigintHandler);
+	}
+	// Create our ROS node. This acts in a similar manner to
+	// the Gazebo node
+	this->rosNode.reset(new ros::NodeHandle(clientName.c_str()));
+	// Create a named topic, and subscribe to it.
+	ros::SubscribeOptions so =
+	  ros::SubscribeOptions::create<nav_msgs::Odometry>(
+	      "/" + this->model_->GetName() + "/setOdometry",
+	      1,
+	      boost::bind(&Odometry::OnRosMsg, this, _1),
+	      ros::VoidPtr(), &this->rosQueue);
+	this->rosSub = this->rosNode->subscribe(so);
+
+	// Spin up the queue helper thread.
+	this->rosQueueThread =
+	  std::thread(std::bind(&Odometry::QueueThread, this));
+	printf("Subscribe %s%s%s\n", "/", this->model_->GetName().c_str(), "/setOdometry");
+
+	this->odom_pub = this->rosNode->advertise<nav_msgs::Odometry>("/"+this->model_->GetName()+"/odom", 50);
+	printf("Publish /%s%s\n", this->model_->GetName().c_str(), "/odom");
+
+	this->current_time = ros::Time::now();
+	this->last_time = ros::Time::now();
+
+}
+
+/** Called by the world update start event
+ */
+void
+Odometry::OnUpdate(const common::UpdateInfo & /*_info*/)
+{
+	//Send position information to Fawkes
+	double time = model_->GetWorld()->GZWRAP_SIM_TIME().Double();
+	if (time - last_sent_time_ > (1.0 / 10.0)) {
+		send_position();
+		last_sent_time_ = time;
+	}
+}
+
+/** on Gazebo reset
+ */
+void
+Odometry::Reset()
+{
+}
+
+/** Functions for recieving Messages (registerd via suscribers)
+ * @param msg message
+ */
+void
+Odometry::on_set_odometry_msg(ConstVector3dPtr &msg)
+{
+	//std::cout << "Got new odometry: " << msg->x() << "|" << msg->y() << "|" << msg->z() << std::endl;
+	{
+		boost::mutex::scoped_lock lock(readingsMutex);
+		estimate_x      = msg->x();
+		estimate_y      = msg->y();
+		estimate_omega  = msg->z();
+		last_sent_time_ = model_->GetWorld()->GZWRAP_SIM_TIME().Double();
+	}
+}
+
+/** Sending position to Fawkes
+ * 
+ */
+void
+Odometry::send_position()
+{
+	gzwrap::Vector3d linearVel  = this->model_->GZWRAP_RELATIVE_LINEAR_VEL();
+	gzwrap::Vector3d angularVel = this->model_->GZWRAP_RELATIVE_ANGULAR_VEL();
+
+	//get the elapsed time since last update
+	double elapsedSeconds = model_->GetWorld()->GZWRAP_SIM_TIME().Double() - last_sent_time_;
+
+	//now add some simulated error
+	// rand multiplied by max noise which is in seconds
+	elapsedSeconds += ((rand() % 2001 - 1000) / 1000.0) * 0.1;
+
+	// we need to project the velocity values from robot base to world base
+	float vecX = linearVel.GZWRAP_X;
+	float vecY = linearVel.GZWRAP_Y;
+
+	//rotate vector by current robot angle
+	float cs = cos(estimate_omega);
+	float sn = sin(estimate_omega);
+	float tx = vecX * cs - vecY * sn;
+	float ty = vecX * sn + vecY * cs;
+
+	// now update robot's position
+	{
+		boost::mutex::scoped_lock lock(readingsMutex);
+		estimate_x += tx * elapsedSeconds;
+		estimate_y += ty * elapsedSeconds;
+		estimate_omega += angularVel.GZWRAP_Z * elapsedSeconds;
+		if (estimate_omega < -M_PI)
+			estimate_omega = 2 * M_PI - estimate_omega;
+		else if (estimate_omega > M_PI)
+			estimate_omega = -2 * M_PI + estimate_omega;
+	}
+
+	if (odometry_pub_->HasConnections()) {
+		//build message
+		msgs::Vector3d posMsg;
+		posMsg.set_x(estimate_x);
+		posMsg.set_y(estimate_y);
+		posMsg.set_z(estimate_omega);
+
+		//send
+		odometry_pub_->Publish(posMsg);
+	}
+
+	ros::spinOnce();
+	this->current_time = ros::Time::now();
+	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(estimate_omega);
+	    
+	//next, we'll publish the odometry message over ROS
+	nav_msgs::Odometry odom;
+    	odom.header.stamp = current_time;
+	odom.header.frame_id = "odom";
+	
+	//set the position
+   	odom.pose.pose.position.x = estimate_x;
+	odom.pose.pose.position.y = estimate_y;
+	odom.pose.pose.position.z = 0.0;
+	odom.pose.pose.orientation = odom_quat;
+
+	//set the velocity
+	odom.child_frame_id = "base_link";
+	odom.twist.twist.linear.x = tx;
+	odom.twist.twist.linear.y = ty;
+	odom.twist.twist.angular.z = angularVel.GZWRAP_Z;
+
+   	//publish the message
+	odom_pub.publish(odom);
+	//printf("publish\n");
+
+}
+
+// \brief Handle an incoming message from ROS
+// \param[in] _msg A float value that is used to set the velocity
+// of the Velodyne.
+// void
+// Motor::OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
+void
+Odometry::OnRosMsg(const nav_msgs::OdometryConstPtr &_msg)
+{
+        // this->SetVelocity(_msg->data);
+	geometry_msgs::PoseWithCovariance  pose_  = _msg->pose;
+	geometry_msgs::TwistWithCovariance twist_ = _msg->twist;
+
+	boost::mutex::scoped_lock lock(readingsMutex);
+ 	estimate_x      = pose_.pose.position.x;
+	estimate_y      = pose_.pose.position.y;
+	// estimate_omega  = pose_.pose.position.z;
+	estimate_omega	= twist_.twist.angular.z;
+	last_sent_time_ = model_->GetWorld()->GZWRAP_SIM_TIME().Double();
+}
+
+/// \brief ROS helper function that processes messages
+void
+Odometry::QueueThread()
+{
+  static const double timeout = 0.01;
+  while (this->rosNode->ok())
+  {
+    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+    printf("%d\n", this->rosNode->ok());
+  }
+}
---
+++ gazebo/odometry/odometry.h
@@ -0,0 +1,100 @@
+/***************************************************************************
+ *  odometry.h - provides odometry simulation of object model
+ *
+ *  Created: Fri Feb 20 18:05:07 2015
+ *  Copyright  2015 Stefan Profanter
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
+#include <boost/bind.hpp>
+#include <boost/thread/mutex.hpp>
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
+#include <list>
+#include <stdio.h>
+#include <string.h>
+
+#include <ros/ros.h>
+// #include </nav_msgs/Odometry.h>
+#include "/opt/ros/noetic/include/nav_msgs/Odometry.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include <thread>
+#include <tf/transform_broadcaster.h>
+
+namespace gazebo {
+/**
+    * Provides odometry simulation of object model
+    * @author Stefan Profanter
+    */
+class Odometry : public ModelPlugin
+{
+public:
+	Odometry();
+	~Odometry();
+
+	//Overridden ModelPlugin-Functions
+	virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/);
+	virtual void OnUpdate(const common::UpdateInfo &);
+	virtual void Reset();
+
+private:
+	/// Pointer to the gazbeo model
+	physics::ModelPtr model_;
+	/// Pointer to the update event connection
+	event::ConnectionPtr update_connection_;
+	///Node for communication to fawkes
+	transport::NodePtr node_;
+	///name of the gps and the communication channel
+	std::string name_;
+
+	///time variable to send in intervals
+	double last_sent_time_;
+
+	//Odometry Stuff:
+
+	///Mutex for thread safe estimate update
+	boost::mutex readingsMutex;
+
+	///Estimated positions
+	float estimate_x;
+	float estimate_y;
+	float estimate_omega;
+
+	///Set odometry callback
+	void on_set_odometry_msg(ConstVector3dPtr &msg);
+
+	///Suscriber for SetOdometry
+	transport::SubscriberPtr set_odometry_sub_;
+
+	///Functions for sending information to fawkes:
+	void send_position();
+
+	///Publisher for Odometry position
+	transport::PublisherPtr odometry_pub_;
+
+	ros::Publisher odom_pub;
+	std::unique_ptr<ros::NodeHandle> rosNode;
+	ros::Subscriber rosSub;
+	ros::CallbackQueue rosQueue;
+	std::thread rosQueueThread;
+	void OnRosMsg(const nav_msgs::OdometryConstPtr &_msg);
+	void QueueThread();
+	ros::Time current_time, last_time;
+	ros::Rate r();
+};
+} // namespace gazebo
